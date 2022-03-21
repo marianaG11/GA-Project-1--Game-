36e3ab365819e99ae26fd0171f9c9cba6fa6c2bd
marianaG11 <mariana.garcia.16@icloud.com>
2022-03-21 14:13:29 -0500

    add event listeners

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 console.log('this is working');
 
 //constants
-const cards = document.querySelectorAll(".cards");
+const cards = document.querySelectorAll('.cards');
 console.log(cards);
-
 //what value to add to current card?
 
 
 //state variables 
 let currentCard;
-let guessedSoFar;
+let guessedSoFar = [];
+let secondCard;
 
 
 
@@ -17,9 +17,30 @@ let guessedSoFar;
 
 
 
+
+
+
+
 //event listeners
 //for start over, timer, mix, begin buttons
+const begin = function() {
+    console.log('game has begun');
+}
+const startOver = function() {
+    console.log('game will start over');
+}
 
+
+
+const flip = (function(){
+    console.log('card is flipped');   
+});
+
+document.querySelector('#begin').addEventListener('click', begin); //for begin and start over buttons
+document.querySelector('#start-over').addEventListener('click', startOver);
+cards.forEach(function(card) {
+    return card.addEventListener('click', flip);
+});
 
 //functions
 
